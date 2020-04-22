0161f4edb195ccc2b6c692cd6a52a50ca3fc54d2
jibuntu <i.am.tomotatu@gmail.com>
2020-04-22 15:48:10 +0900

    一部のコマンドにしか対応していないが、変換結果をファイルにを書き込むように変更

---
+++ Makefile
@@ -0,0 +1,4 @@
+# 他のディレクトリにある問題用の*.vmファイルをコンパイルするためのMakefile
+
+simpleadd:
+	cargo run ../StackArithmetic/SimpleAdd/SimpleAdd.vm ../StackArithmetic/SimpleAdd/SimpleAdd.asm
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 
 use std::env;
 use std::fs::File;
-use std::error::Error;
 use std::io::{Read, Write};
 
 mod parser;
 use parser::Parser;
+use parser::CommandType;
 mod code_writer;
 use code_writer::CodeWriter;
 
@@ -20,6 +20,27 @@ fn print_error(e: &str) {
     print_usage();
 }
 
+/// vmコードを変換してCodeWriterへ保存する。エラーはすべて関数の外に投げ捨てる
+fn vm_to_asm<R, W>(p: &mut Parser<R>, cw: &mut CodeWriter<W>) 
+    -> Result<(), String> where R: Read, 
+                                W: Write {
+    while p.has_more_commands() {
+        p.advance();
+        match p.command_type() {
+            CommandType::PUSH => { 
+                cw.write_push_pop("push", &p.arg1().unwrap(), 
+                                  p.arg2().unwrap())?;
+            },
+            CommandType::ARITHMETIC => {
+                cw.write_arithmetic("add")?;
+            }
+            _ => return Err(format!("{:?} は未実装のコマンドです", 
+                                    p.command_type()))
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     let mut args = env::args().skip(1);
     let filename = match args.next() {
@@ -30,21 +51,19 @@ fn main() {
         Some(f) => f,
         None => return print_error("出力先のファイル名がありません")
     };
-    let mut file = match File::open(&filename) {
+    let file = match File::open(&filename) {
         Ok(f) => f,
         Err(_) => return print_error(&format!("'{}' is not exist.", filename))
     };
+    let outputfile = match File::create(&output_filename) {
+        Ok(f) => f,
+        Err(_) => return print_error(&format!("can't create '{}'.", 
+                                              output_filename))
+    };
 
-//    let mut vm = String::new();
-//    let _ = file.read_to_string(&mut vm);
-//
-//    let mut parser = Parser::new(&vm);
-//    let mut 
-//
-//    while parser.has_more_commands() {
-//        parser.advance();
-//
-//    }
-
-
+    let mut parser = Parser::new(file);
+    let mut code_writer = CodeWriter::new(outputfile);
+    if let Err(e) = vm_to_asm(&mut parser, &mut code_writer) {
+        print_error(&e);
+    }
 }
