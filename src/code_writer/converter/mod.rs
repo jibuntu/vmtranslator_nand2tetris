320229fedd6a62078e089b7be866bd61346ffd24
jibuntu <i.am.tomotatu@gmail.com>
2020-04-22 15:51:39 +0900

    出力するアセンブリコードにコメントを追加した
    出力結果のアセンブリコードを読みやすくなる

--- src/code_writer/converter/mod.rs
+++ src/code_writer/converter/mod.rs
@@ -6,14 +6,16 @@
 /// スタックから2つpopして足し算をする。その結果をスタックに入れる 
 pub fn add() -> String {
     concat!(
+        "// [start] add\n",
         "@SP \n",
         "M=M-1 \n", // SPレジスタの値をデクリメント
         "A=M \n", 
-        "D=M \n",
+        "D=M \n", // Dレジスタに数値を入れる
         "@SP \n",
         "M=M-1 \n", // 再びSPレジスタの値をデクリメント
         "A=M \n",
-        "M=D+M \n" // M[SP] = M[SP+1] + M[SP]
+        "M=D+M \n", // M[SP] = M[SP+1] + M[SP]
+        "// [end] add \n"
     ).to_string()
 }
 
@@ -24,12 +26,14 @@ pub fn push_constant(n: isize) -> String {
     */
     format!(
         concat!(
+            "// [start] push constant {n} \n",
             "@{n} \n", // Aレジスタにnを入れる
             "D=A \n", // Dレジスタに移す
             "@SP \n",
             "A=M \n", // SPレジスタの値をAレジスタに入れる
             "M=D \n", // SPレジスタの値の番地にnを入れる
             "@SP \n",
-            "M=M+1 \n" // SPレジスタの値をインクリメントする
+            "M=M+1 \n", // SPレジスタの値をインクリメントする
+            "// [end] push constant {n} \n",
         ), n=n)
 }
