c429004faf47d212318d26ccbc77036a2629e461
jibuntu <i.am.tomotatu@gmail.com>
2020-04-28 09:30:23 +0900

    test.shでSys.init関数が無いテストのときに-wオプションをつけるように変更

--- test/test.sh
+++ test/test.sh
@@ -25,31 +25,32 @@ get_test_list() {
     result=()
 
     # pash変数の値が""なら配列には何も追加されない
+    # Sys.init関数がない場合は[-w]をつける
     # ".tst"は書かない
     ifarg "staticstest" "../../FunctionCalls/StaticsTest/StaticsTest"; result+=($path)
     ifarg "nestedcall" "../../FunctionCalls/NestedCall/NestedCall"; result+=($path)
     ifarg "fibonaccielement" "../../FunctionCalls/FibonacciElement/FibonacciElement"; result+=($path)
-    ifarg "simplefunction" "../../FunctionCalls/SimpleFunction/SimpleFunction"; result+=($path)
-    ifarg "fibonacciseres" "../../ProgramFlow/FibonacciSeries/FibonacciSeries"; result+=($path)
-    ifarg "basicloop" "../../ProgramFlow/BasicLoop/BasicLoop"; result+=($path)
-    ifarg "if" "./if/test"; result+=($path)
-    ifarg "goto" "./goto/test"; result+=($path)
-    ifarg "push" "./push/test"; result+=($path)
-    ifarg "pop" "./pop/test"; result+=($path)
-    ifarg "lt" "./lt/lt"; result+=($path)
-    ifarg "gt" "./gt/gt"; result+=($path)
-    ifarg "eq" "./eq/eq/eq"; result+=($path)
-    ifarg "eq_true" "./eq/true/true"; result+=($path)
-    ifarg "eq_false" "./eq/false/false"; result+=($path)
-    ifarg "eq_long" "./eq/long/long"; result+=($path)
-    ifarg "add" "./add/add"; result+=($path)
-    ifarg "sub" "./sub/sub"; result+=($path)
+    ifarg "simplefunction" "[-w]../../FunctionCalls/SimpleFunction/SimpleFunction"; result+=($path)
+    ifarg "fibonacciseres" "[-w]../../ProgramFlow/FibonacciSeries/FibonacciSeries"; result+=($path)
+    ifarg "basicloop" "[-w]../../ProgramFlow/BasicLoop/BasicLoop"; result+=($path)
+    ifarg "if" "[-w]./if/test"; result+=($path)
+    ifarg "goto" "[-w]./goto/test"; result+=($path)
+    ifarg "push" "[-w]./push/test"; result+=($path)
+    ifarg "pop" "[-w]./pop/test"; result+=($path)
+    ifarg "lt" "[-w]./lt/lt"; result+=($path)
+    ifarg "gt" "[-w]./gt/gt"; result+=($path)
+    ifarg "eq" "[-w]./eq/eq/eq"; result+=($path)
+    ifarg "eq_true" "[-w]./eq/true/true"; result+=($path)
+    ifarg "eq_false" "[-w]./eq/false/false"; result+=($path)
+    ifarg "eq_long" "[-w]./eq/long/long"; result+=($path)
+    ifarg "add" "[-w]./add/add"; result+=($path)
+    ifarg "sub" "[-w]./sub/sub"; result+=($path)
     # 7章のテスト
-    ifarg "simpleadd" "../../../07/StackArithmetic/SimpleAdd/SimpleAdd"; result+=($path)
-    ifarg "stacktest" "../../../07/StackArithmetic/StackTest/StackTest"; result+=($path)
-    ifarg "basictest" "../../../07/MemoryAccess/BasicTest/BasicTest"; result+=($path)
-    ifarg "pointertest" "../../../07/MemoryAccess/PointerTest/PointerTest"; result+=($path)
-    ifarg "statictest" "../../../07/MemoryAccess/StaticTest/StaticTest"; result+=($path)
+    ifarg "simpleadd" "[-w]../../../07/StackArithmetic/SimpleAdd/SimpleAdd"; result+=($path)
+    ifarg "stacktest" "[-w]../../../07/StackArithmetic/StackTest/StackTest"; result+=($path)
+    ifarg "basictest" "[-w]../../../07/MemoryAccess/BasicTest/BasicTest"; result+=($path)
+    ifarg "pointertest" "[-w]../../../07/MemoryAccess/PointerTest/PointerTest"; result+=($path)
+    ifarg "statictest" "[-w]../../../07/MemoryAccess/StaticTest/StaticTest"; result+=($path)
 
 
 }
@@ -85,17 +86,20 @@ for arg in ${args[@]}; do
     fi
     
     for path in ${result[@]}; do
+        asm_path=$(echo $path | sed -e "s/^\[-w\]*//") # 引数を取り除く
         if [[ $compile == "true" ]]; then
             # コンパイルするときは.vmファイルではなく.vmファイルが入っているディレクトリをコンパイル対象にする
             # したがって、$pathが"./test/vmtest"だったら"./test"がコンパイルする対象になる
             # コンパイル後のファイル名は$path".asm"にする
-            printf "[cmpiled] "
             vm_path=$(echo $path | sed -e "s/[^/]*$//") # ファイル名の部分を取り除く
-            $vmtranslator $vm_path $path".asm"
+            vm_path=$(echo $vm_path | sed -e "s/^\[-w\]*/-w /") # 引数があれば引数を設定する
+            printf "[cmpiled] "
+            $vmtranslator $vm_path $asm_path".asm"
         fi
-        printf $path" : "
+
+        printf $asm_path" : "
         printf "\x1b[31m" # 文字を赤色にする
-        test_result=$(test $path".tst")
+        test_result=$(test $asm_path".tst")
         if [[ $test_result == "End of script - Comparison ended successfully" ]]; then
             printf "\x1b[32m" # 文字を黄緑色にする
             echo $test_result
